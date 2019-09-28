61b10747c831a73a56a4af07da2107f0e668e2fb
moyoonthego <39111972+moyoonthego@users.noreply.github.com>
2019-09-28 17:58:02 -0400

    edit 1

--- src/pages/Landing/Login.jsx
+++ src/pages/Landing/Login.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import './App.css';
 import { LandingPage } from '../../components';
 
 export const Login = () => {
-    return <LandingPage>Login</LandingPage>;
+    return( 
+        <LandingPage>
+            <div class="split left">
+                <div class="centered">
+                    <h2>Hacker Dashboard</h2>
+                </div>
+            </div>
+            <div class="split right">
+                <div class="centered">
+                    <h1>Hack the Valley 4</h1>
+                </div>
+            </div>
+        </LandingPage>
+    );
 };
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,2 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 export * from './Login';
 export * from './Register';
+
+function App() {
+  return (
+    <React.Fragment>
+    </React.Fragment>
+  );
+}
