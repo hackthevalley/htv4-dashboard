77402308bbc0c5ea8a17e5d59331dd1683b732af
Moe Ali <39111972+moyoonthego@users.noreply.github.com>
2019-09-28 23:17:12 -0400

    edit 2

--- src/pages/Landing/Login.jsx
+++ src/pages/Landing/Login.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import './App.css';
+import './index.js';
 import { LandingPage } from '../../components';
+import {Button } from '@cheapreats/react-ui';
 
 export const Login = () => {
     return( 
         <LandingPage>
-            <div class="split left">
-                <div class="centered">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+            <div class="split left" id="leftdesktop">
+                <div class="centered" id="leftdesktop2">
                     <h2>Hacker Dashboard</h2>
+                    <h4>Please login or sign up if you haven't already.</h4>
+                    <form name="sendlogininfo">
+                        <h3>Email</h3> 
+                        <input type="text" name="email" placeholder="  Your hacker email"/><br/>
+                        <h3>Password</h3>
+                        <input type="password" name="passw" placeholder="  Your secret salt"/><br/> <br/>
+                        <Button style="black=false;"></Button> <Button text="Sign Up" value="Sign Up" name="signup" id="signup"></Button>
+                    </form>
                 </div>
             </div>
-            <div class="split right">
+            <div class="split right" id="rightdesktop">
                 <div class="centered">
                     <h1>Hack the Valley 4</h1>
                 </div>
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export * from './Login';
-export * from './Register';
-
 function App() {
   return (
     <React.Fragment>
     </React.Fragment>
   );
 }
+
+// HIDE SPLIT SCREEN VIEW FOR MOBILE
+var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+if (isMobile) {
+  //document.getElementById("leftdesktop").disabled = true; NOT WORKING, returns null bcz wrong page
+ }
+
+export * from './Login';
+export * from './Register';
